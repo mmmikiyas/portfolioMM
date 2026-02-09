f4d12138078311f06d51de7c7d144a71c41c0901
mmmikiyas <compoddu@gmail.com>
2026-02-08 20:18:20 -0800

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+export default function Footer() {
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        <a href="#!">
+          <img
+            src="/assets/logo.png"
+            alt=""
+            className="w-36 mx-auto mb-2 dark:hidden"
+          />
+          <img
+            src="/assets/logo.png"
+            alt=""
+            className="w-36 mx-auto mb-2 hidden dark:block"
+          />
+        </a>
+
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <img
+            src="./assets/mail_icon.png"
+            alt=""
+            className="w-5 dark:hidden"
+          />
+          <img
+            src="./assets/mail_icon_dark.png"
+            alt=""
+            className="w-5 hidden dark:block"
+          />
+
+          <a href="mailto:contact@prebuiltui.com">compoddu@gmail.com</a>
+        </div>
+      </div>
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-3">
+        <p>
+          © {new Date().getFullYear()}{" "}
+          <a href="https://prebuiltui.com" target="_blank">
+            MikiyasTech
+          </a>{" "}
+          • All rights reserved.
+        </p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a href="https://github.com/mmmikiyas">GitHub</a>
+          </li>
+          <li>
+            <a href="https://t.me/Mikiyas_M">Telegram</a>
+          </li>
+          <li>
+            <a href="https://web.facebook.com/mikiyas.mekonnen1">facebook</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
